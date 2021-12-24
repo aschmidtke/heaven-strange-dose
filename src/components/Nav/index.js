ef4d64a1242415792bea4941b40b99d91ef17f08
aschmidtke <87831418+aschmidtke@users.noreply.github.com>
2021-12-23 19:58:19 -0500

    styling, resume

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,33 +3,33 @@ import React from 'react';
 function Nav(props) {
     const tabs = ['About', 'Portfolio', 'Contact', 'Resume'];
     return (
-        <ul className="row">
-            <h2 className="col-md-4">
-                <a className="py-2" href="/">
-                    <span className="headerIcon">
-                        <i class="bi bi-slash"></i>
-                    </span>
-                    Drew Schmidtke
-                </a>
-            </h2>
-            {tabs.map(tab => (
-                <li className="col-md-2 py-2" key={tab}>
-                    <h4>
+            <ul className="row">
+                <h2 className="col-md-4">
+                    <a className="py-2" href="/">
                         <span className="headerIcon">
                             <i class="bi bi-slash"></i>
                         </span>
-                        <a
-                            href={'#' + tab.toLowerCase()}
-                            onClick={() => props.handlePageChange(tab)}
-                        // className={props.currentPage === tab ? 'nav-link active' : 'nav-link'}
-                        >
-                            {tab}
-                        </a>
-                    </h4>
-                </li>
-            ))}
+                        Drew Schmidtke
+                    </a>
+                </h2>
+                {tabs.map(tab => (
+                    <li className="col-md-2 py-2" key={tab}>
+                        <h4>
+                            <span className="headerIcon">
+                                <i class="bi bi-slash"></i>
+                            </span>
+                            <a
+                                href={'#' + tab.toLowerCase()}
+                                onClick={() => props.handlePageChange(tab)}
+                            // className={props.currentPage === tab ? 'nav-link active' : 'nav-link'}
+                            >
+                                {tab}
+                            </a>
+                        </h4>
+                    </li>
+                ))}
 
-        </ul>
+            </ul>
     );
 }
 
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,9 +1,14 @@
 import React from "react";
+import resume from "../../assets/resume.jpg"
 
 function Resume() {
     return(
-        <div className="text-center">
-            I am the resume!
+        <div className="text-center containerColor">
+            <img src={resume} alt="resume photo" />
+            <br></br>
+            <a className="headerIcon" href={resume} download>
+                <i class="bi bi-download" />
+            </a>
         </div>
     );
 }
